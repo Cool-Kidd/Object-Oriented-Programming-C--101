74b00e5d19a7db903350752d50b74822270e2602
Austin <g42pixels@gmail.com>
2022-02-14 09:34:20 +0200

    Created BankAccount Object

---
+++ MySuperBank/MySuperBank/BankAccount.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MySuperBank
+{
+    public class BankAccount
+    {
+        public string Number { get;  }
+        public string Owner { get; set; }
+        public decimal Balance { get; }
+
+        public BankAccount(string name, decimal intitialBalance)
+        {
+            Owner = name;
+            Balance = intitialBalance;  
+        }
+
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+        }
+
+        public void MakeWithdrawal(decimal amount, DateTime date, string note)
+        {
+        }
+    }
+}
--- MySuperBank/MySuperBank/Program.cs
+++ MySuperBank/MySuperBank/Program.cs
@@ -10,6 +10,8 @@ namespace MySuperBank
     {
         static void Main(string[] args)
         {
+            var account = new BankAccount("Austin",10000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance}");
         }
     }
 }
